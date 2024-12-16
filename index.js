859a94c92cee6da1fd15d805d7e11899829886ab
RobAndonie <a01411863@tec.mx>
2024-12-16 08:57:26 +0100

    Error mesages

--- index.js
+++ index.js
@@ -43,11 +43,17 @@ const errorHandler = (error, req, res, next) => {
   console.error(error.message);
 
   if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
+    return res
+      .status(400)
+      .send({ error: `Malformatted id, ${error.message} ` });
   } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: "validation error" });
+    return res
+      .status(400)
+      .json({ error: `Validation error, ${error.message}` });
   } else {
-    return res.status(500).json({ error: "internal server error" });
+    return res
+      .status(500)
+      .json({ error: `Internal server error, ${error.message}` });
   }
 };
 
